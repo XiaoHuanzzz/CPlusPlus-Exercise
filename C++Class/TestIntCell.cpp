049e5547a8193a337ef918848db80de2d23d2ec5
XiaoHuanzzz <140334171+XiaoHuanzzz@users.noreply.github.com>
2024-04-03 10:19:23 +0800

    Update TestIntCell.cpp

--- C++Class/TestIntCell.cpp
+++ C++Class/TestIntCell.cpp
@@ -2,7 +2,7 @@
  * @Author: (xiao_huan) 1156492597@qq.com
  * @Date: 2024-04-02 21:47:58
  * @LastEditors: (xiao_huan) 1156492597@qq.com
- * @LastEditTime: 2024-04-02 21:51:23
+ * @LastEditTime: 2024-04-03 10:14:30
  * @FilePath: \CPlusPlus Exercise\C++Class\TestIntCell.cpp
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  */
@@ -11,8 +11,10 @@
 using namespace std;
 
 int main(){
-    IntCell m;
-    m.write(5);
-    cout << m.read();
+    IntCell *m;
+    m = new IntCell(10);
+    m->write(5);
+    cout << m->read();
+    delete m;
     return 0;
 }
